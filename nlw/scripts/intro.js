76dc5498aaabe931e4f784b65d531b34922595e9
Heitor <heitorssx2@gmail.com>
2020-06-04 14:29:48 -0300

    Aula 2: 02/06/2020

---
+++ nlw/scripts/intro.js
@@ -0,0 +1,69 @@
+// document.write("Hello")
+
+// variaveis , tipos de dados
+// let myvar ="Hello"
+
+// const myconst = "he"
+
+// document.write(myconst+myvar)
+
+// String
+// const s1 = "Isso é uma string"
+// const s2 ='Isso é uma string'
+// const s3 = `Isso é uma string
+
+//Number
+// const n1 = 1
+// const n2 = 12
+// document.write(n1+n2)
+
+// boolean - true ou false
+// const bTrue = true
+// const bFalse = false
+// document.write(bFalse)
+
+// Objeto possuem propriedades e funcionalidades
+// const pessoa = {
+//   altura:"1,80m",
+//   idade: 24,
+//   solteiro: true,
+//   correr(){
+//     document.write("executar uma grande logica de correr")
+//   }
+// }
+
+// pessoa.correr()
+
+
+
+// Arry - vetores
+// const colecaoDeBolinhas = ["blue", "gree", 1, {name:"My name"}]
+
+// document.write(colecaoDeBolinhas[3].name)
+
+
+// Funções - Funcionalidades - Atalhos - Reuso de código
+
+// function sayMyName(name) {
+//   document.write(name)
+// }
+
+//Executar
+// sayMyName("Beltrano")
+// sayMyName("Ciclano")
+// sayMyName("Fulano")
+
+
+// Condicionais
+// const notaFinal = 7
+// if(notaFinal< 5){
+//      document.write("Reprovado")
+//    } else {
+//      document.write("Aprovado")
+//    }
+
+
+// Loop - repetições
+for(i=0; i<10;i++){
+  document.write(`<p>${i}</p>`)
+}
